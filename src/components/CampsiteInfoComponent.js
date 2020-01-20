ce962e11f5674a42ddc85d1d9241912eccc7f5bb
Christine Lamitina <christinelamitina@gmail.com>
2020-01-20 11:40:54 -0600

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -18,8 +18,8 @@ class CommentForm extends Component {
     }
 
     handleSubmit(values) {
-        console.log('Current state is: ' + JSON.stringify(values));
-        alert('Current state is: ' + JSON.stringify(values));
+        this.toggleModal();
+        this.props.addComment(this.props.campsiteId, values.rating, values.name, values.feedback);
     }
 
     render(){
@@ -97,7 +97,7 @@ function RenderCampsite({campsite}) {
         );
     }
 
-function RenderComments({comments}) {
+function RenderComments({comments, addComment, campsiteId}) {
         if (comments) {
             return(
                     <div className="col-md-5 m-1">
@@ -109,7 +109,7 @@ function RenderComments({comments}) {
                                 </div>
                             );
                         })}
-                         <CommentForm />
+                        <CommentForm campsiteId={campsiteId} addComment={addComment} />
                     </div>
             );
         }
@@ -134,7 +134,11 @@ function CampsiteInfo(props) {
                     </div>
                     <div className="row">
                         <RenderCampsite campsite={props.campsite} />
-                        <RenderComments comments={props.comments} />
+                        <RenderComments 
+                        comments={props.comments}
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id}
+                    />
                     </div>
                 </div>
             );
