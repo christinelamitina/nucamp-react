0aba9ffe7d1357e2bc4222d6f41a4f96379e7928
Christine Lamitina <christinelamitina@gmail.com>
2020-01-25 11:38:06 -0600

    week 1

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
 
 function About(props) {
@@ -102,7 +103,7 @@ function PartnerList(props){
         );
     }
 
-    else () {
+
         return (
             <div className = "col-mt-4">
                 <Media list>
@@ -110,7 +111,7 @@ function PartnerList(props){
                     </Media>
             </div>
         );
-    }
-
+   
+}
 
 export default About;
